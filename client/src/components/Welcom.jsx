de957c8c25fc73d4078b11141bdf4fb453d1f3a9
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2022-05-08 21:38:37 +0200

    chore(ui): added basic ui

--- client/src/components/Welcom.jsx
+++ client/src/components/Welcom.jsx
@@ -7,13 +7,13 @@ import { Loader } from './';
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120] flex justify-center items-center border-[0.5px] border-gray-400 text-white';
 
 const Input = ({placeholder, name, type, value, handleChange}) => {
-    <input 
+    return <input 
      placeholder={placeholder}
      type={type}
      step="0.0001"
      value={value}
      onChange={(e) => handleChange(e, name)}
-     className="my-2 w-full rounded-sm outline-none bg-transparent text-white border-none text-sm white-glassmorpism  "
+     className="my-2 w-full rounded-sm outline-none bg-transparent p-2 text-white border-none text-sm white-glassmorphism  "
     />
 }
 
@@ -22,10 +22,14 @@ const Welcome = () => {
 
     }
 
+    const handleSubmit = () => {
+
+    }
+
     return (
         <div className="flex w-full justify-center items-center">
-            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 px-4">
-                <div className="flex flex-1 justify-start flex-col md:mr-10">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 px-4">
+                <div className="flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">Send Cryto <br/> accross the world</h1>
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy and sell crypto currencies easily on Krypto. </p>
                     <button 
@@ -57,7 +61,7 @@ const Welcome = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+                <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
                     <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
@@ -78,6 +82,23 @@ const Welcome = () => {
                     </div>
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
                         <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
+                        <Input placeholder="Amount (ETH)" name="amountTo" type="number" handleChange={() => {}} />
+                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
+                        <Input placeholder="Enter message" name="message" type="text" handleChange={() => {}} />
+
+                        <div className="h-[1px] w-full bg-gray-400 my-2" />
+                        {true ? (
+                            <Loader />
+                        ):
+                        (
+                            <button type="button" 
+                                onClick={handleSubmit}
+                                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                            >
+                                Send Now
+                            </button>
+                        )
+                        }
                     </div>
                 </div>
 
